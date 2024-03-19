4a133f391f8a3e41eff217628967eee4db5021d4
miko <miko@aquaform.hu>
2024-03-19 15:40:47 +0100

    trying...

--- projects/nm-splitter/src/public-api.ts
+++ projects/nm-splitter/src/public-api.ts
@@ -2,7 +2,10 @@
  * Public API Surface of nm-splitter
  */
 
+export * from './lib/nm-splitter-divider/nm-splitter-divider.component';
 export * from './lib/nm-splitter.component';
 export * from './lib/nm-splitter.module';
 export * from './lib/nm-splitter.service';
+export * from './lib/nm-splittertab/nm-splittertab.component';
+export * from './lib/nm-tab/nm-tab.component';
 
